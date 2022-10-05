16c9ee7390ea81d3bb917f7e8a3a3d92c13361bd
Nagy Botond <botondnagy98@icloud.com>
2022-10-05 14:48:03 +0300

    Day 2: Dive, Part 2 complete

--- rust/_2021-day-2/src/main.rs
+++ rust/_2021-day-2/src/main.rs
@@ -1,13 +1,19 @@
+#![allow(unused)]
+
 use std::{error::Error, fs};
 
-fn main() -> Result<(), Box<dyn Error>> {
-  let filename = "dataset.txt";
-  let dataset = fs::read_to_string(filename)?;
-
-  let parsed = dataset
+fn parse_dataset<'a>(dataset: &'a String) -> Vec<Vec<&'a str>> {
+  dataset
     .lines()
     .map(|line| line.split(" ").collect::<Vec<_>>())
-    .collect::<Vec<_>>();
+    .collect::<Vec<_>>()
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let filename1 = "dataset.txt";
+  let dataset1 = fs::read_to_string(filename1)?;
+
+  let parsed = parse_dataset(&dataset1);
 
   // Start of Part 1
   {
@@ -18,10 +24,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut depth = 0;
 
     for vec in parsed.iter() {
-      if let [instruction, value] = vec.as_slice() {
+      if let [instruction, value] = *vec.as_slice() {
         let value = value.parse::<i32>()?;
 
-        match *instruction {
+        match instruction {
           "forward" => forward += value,
           "up" => depth -= value,
           "down" => depth += value,
@@ -37,7 +43,36 @@ fn main() -> Result<(), Box<dyn Error>> {
   // End of Part 1
 
   // Start of Part 2
-  {}
+  let filename2 = "dataset2.txt";
+  let dataset2 = fs::read_to_string(filename2)?;
+
+  {
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut forward = 0;
+
+    let parsed = parse_dataset(&dataset2);
+
+    for vec in parsed.iter() {
+      if let [instruction, value] = *vec.as_slice() {
+        let value = value.parse::<i32>()?;
+
+        match instruction {
+          "down" => aim += value,
+          "up" => aim -= value,
+          "forward" => {
+            forward += value;
+            depth += aim * value;
+          }
+          _ => unreachable!(),
+        }
+      }
+    }
+
+    let final_position = forward * depth;
+
+    println!("{}", final_position);
+  }
   // End of Part 2
 
   Ok(())
