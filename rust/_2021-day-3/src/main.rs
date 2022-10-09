f591985c3e97766c0791cab4d8a1f890d6df03b8
Nagy Botond <botondnagy98@icloud.com>
2022-10-09 14:37:29 +0300

    Hardest goddamn problem honestly (so far)

--- rust/_2021-day-3/src/main.rs
+++ rust/_2021-day-3/src/main.rs
@@ -1,12 +1,14 @@
-#![allow(unused_mut, unused_variables)]
+#![allow(unused_variables, unused_mut)]
 
 use std::{collections::BTreeMap, error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let filename = "dataset1.txt";
-  let dataset1 = fs::read_to_string(filename)?;
+  // Start of Part 1
+  let filename = "dataset.txt";
+  let dataset = fs::read_to_string(filename)?;
 
-  let parsed = dataset1
+  // parse the data into a nested array
+  let parsed = dataset
     .lines()
     .map(|line| {
       line
@@ -24,28 +26,39 @@ fn main() -> Result<(), Box<dyn Error>> {
   let mut ones = 0;
   let mut btreemap: BTreeMap<usize, (u32, u32)> = BTreeMap::new();
 
-  for pos in 0..=11 {
+  // traverse the top level array vertically
+  // on each round, using only the first element of the subarrays
+  // do this until
+  for pos in 0..=parsed[0].len() - 1 {
     for (index, vec) in parsed.iter().enumerate() {
-      let digit_col = vec.iter().nth(pos).unwrap();
+      // iterate over the columns one by one
+      let digit_col = vec
+        .iter()
+        .nth(pos)
+        .expect("Length of sub array is more than expected");
 
       match digit_col {
         0 => zeroes += 1,
         1 => ones += 1,
-        _ => unreachable!(),
+        // only ones and zeroes in this array, this case should never be matched
+        _ => unreachable!("Not just ones and zeroes in the sub arrays, panic!"),
       }
 
+      // reset the intermediate variables after each iteration
+      // but save the results into a btreemap before that
+      // (using a btreemap because order is relevant)
       if index == parsed.len() - 1 {
-        btreemap.insert(pos + 1, (zeroes, ones));
+        btreemap.insert(pos, (zeroes, ones));
         zeroes = 0;
         ones = 0;
       }
     }
   }
 
-  let mut gamma_rate = String::from("");
-  let mut epsilon_rate = String::from("");
+  let mut gamma_rate = String::with_capacity(12);
+  let mut epsilon_rate = String::with_capacity(12);
 
-  for (_, (zeroes, ones)) in btreemap.into_iter() {
+  for (_, (zeroes, ones)) in btreemap.iter() {
     if zeroes > ones {
       gamma_rate.push('0');
       epsilon_rate.push('1');
@@ -55,16 +68,80 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
   }
 
+  // get the decimal value of the binary numbers
   let decimal_gamma_rate = usize::from_str_radix(&gamma_rate, 2)?;
   let decimal_epsilon_rate = usize::from_str_radix(&epsilon_rate, 2)?;
-
-  println!("Gamma rate in decimal: {}", decimal_gamma_rate);
-  println!("Epsilon rate in decimal: {}", decimal_epsilon_rate);
 
   println!(
     "Power consumption: {}",
     decimal_gamma_rate * decimal_epsilon_rate
   );
+  // End of Part 1
+
+  // Start of Part 2
+  {
+    let mut oxygen_rating = dataset.lines().collect::<Vec<_>>();
+    let mut co2_rating = dataset.lines().collect::<Vec<_>>();
+
+    for position in 0..oxygen_rating.first().unwrap().len() {
+      let zeroes = dbg!(count(&oxygen_rating, '0', position));
+      let ones = dbg!(count(&oxygen_rating, '1', position));
+
+      if zeroes > ones {
+        dbg!(keep_elements(&mut oxygen_rating, '1', position));
+      } else if zeroes < ones {
+        dbg!(keep_elements(&mut oxygen_rating, '0', position));
+      } else if zeroes == ones {
+        dbg!(keep_elements(&mut oxygen_rating, '0', position));
+      }
+
+      if oxygen_rating.len() == 1 {
+        break;
+      }
+    }
+
+    for position in 0..co2_rating.first().unwrap().len() {
+      let zeroes = dbg!(count(&co2_rating, '0', position));
+      let ones = dbg!(count(&co2_rating, '1', position));
+
+      if zeroes > ones {
+        dbg!(keep_elements(&mut co2_rating, '0', position));
+      } else if zeroes < ones {
+        dbg!(keep_elements(&mut co2_rating, '1', position));
+      } else if zeroes == ones {
+        dbg!(keep_elements(&mut co2_rating, '1', position));
+      }
+
+      if co2_rating.len() == 1 {
+        break;
+      }
+    }
+
+    dbg!(&oxygen_rating);
+    dbg!(&co2_rating);
+
+    let first = u128::from_str_radix(oxygen_rating.first().unwrap(), 2)?;
+    let second = u128::from_str_radix(co2_rating.first().unwrap(), 2)?;
+
+    dbg!(first);
+    dbg!(second);
+
+    dbg!(first * second);
+  }
+  // End of Part 2
 
   Ok(())
+}
+
+/// Count `character` of `vector` at `position
+fn count(vector: &Vec<&str>, character: char, position: usize) -> usize {
+  vector
+    .iter()
+    .filter(|&&string| string.chars().nth(position) == Some(character))
+    .count()
+}
+
+/// Keep lines in `vector` where `character` matches char at `position`
+fn keep_elements(vector: &mut Vec<&str>, character: char, position: usize) {
+  vector.retain(|&element| element.chars().nth(position) == Some(character))
 }
